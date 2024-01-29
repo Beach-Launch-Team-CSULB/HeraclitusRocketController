1f20b76e51bb1082f40f4de5ca0d7d7cdfd03593
analiciasosa2019 <115589709+analiciasosa2019@users.noreply.github.com>
2024-01-29 13:22:58 -0800

    added CAN class structure and some functions

--- CANDriver.h
+++ CANDriver.h
@@ -1,6 +1,14 @@
 #include <FlexCAN_T4.h>
 
-// readCAN()
-// writeCAN()
+class CANDriver {
+    private:
+        bool isFD
+    public:
+        CANDriver(bool)
+        // returnVal write(uint8_t)
+        // returnVal read(uint8_t)
+        bool getIsFD()
+}
+
 // This software needs to have support for both CAN2.0 and CANFD. 
 // The imported library has support for both
