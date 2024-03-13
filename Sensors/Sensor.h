720f164fc2a21f260fa7ce361810e543871022b0
Aurum <47676248+XZ-Crazy@users.noreply.github.com>
2024-03-12 17:59:44 -0700

    Edited Sensor Bitmasks
    Added missing PTs and reassigned TC and LC IDs, leaving room for up to 12 TCs

--- Sensors/Sensor.h
+++ Sensors/Sensor.h
@@ -6,26 +6,31 @@
 //TODO: move ID definitions to config.h after branch is merged with main
 
 //Each int is 32 bits, meaning we can have 32 unique sensor bitmasks
-#define PT_LOX_HIGH_ID (1<<0)   //00000000 00000001
-#define PT_FUEL_HIGH_ID (1<<1)  //00000000 00000010
-#define PT_LOX_DOME_ID (1<<2)   //00000000 00000100
-#define PT_FUEL_DOME_ID (1<<3)  //00000000 00001000
-#define PT_LOX_TANK_1_ID (1<<4) //00000000 00010000
-#define PT_LOX_TANK_2_ID (1<<5) //00000000 00100000
-#define PT_FUEL_TANK_1_ID (1<<6)//00000000 01000000
-#define PT_FUEL_TANK_1_ID (1<<7)//00000000 10000000
-#define PT_LOX_INLET_ID (1<<8)  //00000001 00000000
-#define PT_FUEL_INLET_ID (1<<9) //00000010 00000000
+#define PT_LOX_HIGH_ID (1<<0)      //00000000 00000001
+#define PT_FUEL_HIGH_ID (1<<1)     //00000000 00000010
+#define PT_LOX_DOME_ID (1<<2)      //00000000 00000100
+#define PT_FUEL_DOME_ID (1<<3)     //00000000 00001000
+#define PT_LOX_TANK_1_ID (1<<4)    //00000000 00010000
+#define PT_LOX_TANK_2_ID (1<<5)    //00000000 00100000
+#define PT_FUEL_TANK_1_ID (1<<6)   //00000000 01000000
+#define PT_FUEL_TANK_1_ID (1<<7)   //00000000 10000000
+#define PT_PNUEMATICS (1<<8)       //00000001 00000000
+#define PT_LOX_INLET_ID (1<<9)     //00000010 00000000
+#define PT_FUEL_INLET_ID (1<<10)   //00000100 00000000
+#define PT_FUEL_INJECTOR_ID (1<<11)//00001000 00000000
+#define PT_CHAMBER_1_ID (1<<12)    //00010000 00000000
+#define PT_CHAMBER_2_ID (1<<13)    //00100000 00000000
 
 #define TC_1 (1<<16)
 #define TC_2 (1<<17)
 #define TC_3 (1<<18)
 #define TC_4 (1<<19)
+//We might need up to 12 thermocouples placed in groups of 3
 
-#define LC_1 (1<<24)
-#define LC_2 (1<<25)
-#define LC_3 (1<<26)
-#define LC_4 (1<<27)
+#define LC_1 (1<<28)
+#define LC_2 (1<<29)
+#define LC_3 (1<<30)
+#define LC_4 (1<<31)
 
 class Sensor {
 private:
