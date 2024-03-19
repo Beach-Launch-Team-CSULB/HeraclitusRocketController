d6e6484be6b27f40a3791b0e8f25c0f8e777bcd9
bonniellhwhite <bonniellhwhite@gmail.com>
2024-03-18 17:58:23 -0700

    Custom Testing Valve

--- ExtendedIO.cpp
+++ ExtendedIO.cpp
@@ -74,17 +74,8 @@ void ExtendedIO::pinModeExtended(int pin, int isGPIO, int dataDirection) {
     /*  Argument 1: Pin # designation specified on the ALARA V2.x MCU Pin Map and gets the address 
         Argument 2: Specifies if this is a General Purpose Input/Output pin or not 
         Manually writes to Registers that define given pin behaviors
-    */
     
-    uint32_t PCR_address = fetchRegister(pin,PCR);     // Gets the exact address of the pin control register
-    if(PCR_address != uint32_t(0)){                             // If it is a valid Pin with an address 
-        if(isGPIO == 1){
-            (*(volatile uint32_t*) PCR_address) = PORT_PCR_MUX(0x1);                          // Sets pin PCR to GPIO Mode, 0 is Analog/Disabled, 1 is GPIO
-        }else{
-            (*(volatile uint32_t*) PCR_address) = PORT_PCR_MUX(0x0);                        // Sets pin PCR to Analog/Disabled, 1 is GPIO
-        }
-    }
-
+    
     uint32_t PDDR_address = fetchRegister(pin, PDDR);  // Gets the exact address of the Port Data Direction Register
     if(PDDR_address != 0){
         int PDDR_Offset = digitalPinToBit_int(pin);          // Gets bit offset for PDDR Register 
@@ -95,13 +86,27 @@ void ExtendedIO::pinModeExtended(int pin, int isGPIO, int dataDirection) {
             (*(volatile uint32_t*)PDDR_address) = (0 << PDDR_Offset);                 // Sets pin PDDR to 0 at correct bit offset
         }
     }
+
+    uint32_t PCR_address = fetchRegister(pin,PCR);     // Gets the exact address of the pin control register
+    if(PCR_address != uint32_t(0)){                                                // If it is a valid Pin with an address 
+        if(isGPIO == 1){
+            (*(volatile uint32_t*) PCR_address) = (1<<8);                          // Sets pin PCR to GPIO Mode, 0 is Analog/Disabled, 1 is GPIO
+        }else{
+            (*(volatile uint32_t*) PCR_address) = (0<<8);                          // Sets pin PCR to Analog/Disabled, 1 is GPIO
+        }
+    }
+    */
+
+   //testing for LV
+   (*(volatile uint32_t *)0x4004C028) = (1<8); //PCR
+   (*(volatile uint32_t *)0x400FF0D4) = (1<10); //PDDR
 }
 
 void ExtendedIO::digitalWriteExtended(int pin, int value) {
     /*  Argument 1: Pin # designation specified on the ALARA V2.x MCU Pin Map and gets the address 
         Argument 2: 1 High, 0 Low
         Manually Writes Commands to Ports, Pinmode must be set prior
-    */
+    
     int pinBitOffset = digitalPinToBit_int(pin);                   // Gets bit offset for both PCOR & PSOR Register 
     if (value == 0){  // Clear port to 0: Low
         uint32_t PCOR_address = fetchRegister(pin,PCOR);     // Gets the exact address of the Port Clear Output Register
@@ -115,5 +120,7 @@ void ExtendedIO::digitalWriteExtended(int pin, int value) {
             (*(volatile uint32_t*)PSOR_address) = (1 << pinBitOffset);                          // Sets PDOR bit to 1 (High) at correct bit offset
         }
     }
+    */
+    (*(volatile uint32_t *)0x400FF0C0) = (value<10); //PDOR
 }
 
